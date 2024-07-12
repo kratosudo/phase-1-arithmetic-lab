6ce84b2de30d02fee48d8bf7523221a662de6163
kibiruki <+>
2024-07-12 12:47:38 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+//multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
+
+//random
+ let random = Math.floor(Math.random() * 100) + 1
+ console.log(random);
+
+ //mod
+ let num3 = 10;
+ let num4 = 6;
+ let mod = num3 % num4;
+ console.log(mod);
+
+ //max
+ let max = (1,3,12,15,20);
+ console.log(max);
+
---
+++ notex.js
@@ -0,0 +1,4 @@
+// let num1 = 31;
+// let num2 = 2;
+// let multiply =(num1 * num2) ;
+// console.log(multiply);
